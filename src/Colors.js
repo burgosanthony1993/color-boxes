9d73239c59cd82596ae9f8f2982b581135068564
Anthony Burgos <burgosanthony1993@gmail.com>
2024-09-16 21:41:06 -0400

    first commit

---
+++ src/Colors.js
@@ -0,0 +1,14 @@
+import react, {Component} from 'react'
+import ColorSquare from './colorBox'
+
+class Color extends Component {
+    render (){
+        return (
+            <div> 
+                <ColorSquare/>
+            </div>
+        )
+    }
+}
+
+export default Color;
---
+++ src/colorBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './colorBox.css';
+
+class ColorSquare extends Component {
+    render() {
+        return (
+            <div className='Color'></div> 
+        );
+    }
+}
+
+export default ColorSquare; // Correct name for exporting the component
