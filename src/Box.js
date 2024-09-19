ba6ac4389a8c8abcc2fda918d537f83eaf0d6c36
Anthony Burgos <burgosanthony1993@gmail.com>
2024-09-19 11:18:34 -0400

    child component, Box.js is complete

--- src/Box.js
+++ src/Box.js
@@ -4,25 +4,34 @@ import './Box.css';
 
 class Box extends Component {
     static defaultProps = {
-        allColors: ['purple', 'magenta', 'lilac', 'pink']
-    }
+        allColors: ['purple', 'magenta', 'violet', 'pink', 'blue', 'orange']
+    } //sets the default colors
 
-    constructor(props){
+    constructor(props){ //initalizes the state with a random color and binds the event handler
         super(props);
         this.state = {color: choice(props.allColors)}
         this.handleClick = this.handleClick.bind(this);
     }
+
+    pickColor(){
+        let newColor; // chooses a new color different from the current and sets state
+        do {
+            newColor = choice(this.props.allColors); // Assign the result of choice to newColor
+        } while (newColor === this.state.color);
+        this.setState({color: newColor});
+    }
+    
+
     handleClick (){
+        this.pickColor() //calls pickColor when clicked
+    } 
 
-    } // function logic goes in here
-
-    render (){
+    render (){ //defines what gets displayed on scene
         return(
             <div className='Box' style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
-
             </div>
         )
     }
 }
 
-export default Box; // Correct name for exporting the component
+export default Box; // Correct name for exporting this child component
